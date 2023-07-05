11ba5f37f129c18d81e5c0724f3aad38d5ee9462
Stepan Rudenko <s.rudenko584@gmail.com>
2023-07-05 14:21:52 +0300

    Render posts on profile page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,11 @@ dayjs.extend(relativeTime);
 import toast from "react-hot-toast";
 
 import { api } from "~/utils/api";
-import type { RouterOutputs } from "~/utils/api";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
-import Link from "next/link";
 import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -75,37 +74,6 @@ const CreatePostWizard = () => {
           <LoadingSpinner size={24} />
         </div>
       )}
-    </div>
-  );
-};
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-
-  return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        src={author.profileImageUrl}
-        alt={`@${author.username}'s profile picture`}
-        className="h-14 w-14 rounded-full"
-        height={56}
-        width={56}
-      />
-      <div className="flex flex-col">
-        <div className="flex gap-2 text-slate-300">
-          <Link href={`/@${author.username}`}>
-            <span className="font-extrabold">{`@${author.username}`}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">{`| ${dayjs(
-              post.createdAt
-            ).fromNow()}`}</span>
-          </Link>
-        </div>
-        <span className="text-xl">{post.content}</span>
-      </div>
     </div>
   );
 };
